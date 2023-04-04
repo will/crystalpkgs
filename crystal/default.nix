cafe263d0ca86beb96c70f61f95c2f36b9531d0c
Will Leinweber <will@bitfission.com>
2023-04-04 17:14:21 +0200

    build crystal package

--- crystal/default.nix
+++ crystal/default.nix
@@ -2,6 +2,7 @@
 , lib
 , src
 , substituteAll
+, callPackage
   # build deps
 , llvm
 , crystal_prebuilt
@@ -28,62 +29,68 @@
   # options
 , release ? false
 }:
-stdenv.mkDerivation rec {
-  name = "crystal";
-  inherit src;
-  inherit (stdenv) isDarwin;
+lib.fix (compiler:
+  stdenv.mkDerivation rec {
+    name = "crystal";
+    inherit src;
+    inherit (stdenv) isDarwin;
 
-  disallowedReferences = [ crystal_prebuilt ];
-  enableParallelBuilding = true;
-  outputs = [ "out" "lib" "bin" ];
-  strictDeps = true;
+    passthru = {
+      buildCrystalPackage = callPackage ./build-crystal-package.nix { crystal = compiler; };
+    };
 
-  nativeBuildInputs = [ makeWrapper installShellFiles crystal_prebuilt ];
+    disallowedReferences = [ crystal_prebuilt ];
+    enableParallelBuilding = true;
+    outputs = [ "out" "lib" "bin" ];
+    strictDeps = true;
 
-  buildInputs = [ boehmgc gmp libevent libffi libxml2 libyaml openssl pcre2 zlib ]
-    ++ lib.optionals isDarwin [ libiconv ];
+    nativeBuildInputs = [ makeWrapper installShellFiles crystal_prebuilt ];
 
-  patches = [ (substituteAll { src = ./tzdata.patch; inherit tzdata; }) ];
+    buildInputs = [ boehmgc gmp libevent libffi libxml2 libyaml openssl pcre2 zlib ]
+      ++ lib.optionals isDarwin [ libiconv ];
 
-  postPatch = ''
-    substituteInPlace Makefile \
-      --replace 'CRYSTAL_CONFIG_BUILD_COMMIT :=' 'CRYSTAL_CONFIG_BUILD_COMMIT ?=' \
-      --replace 'SOURCE_DATE_EPOCH :=' 'SOURCE_DATE_EPOCH ?='
-  '';
+    patches = [ (substituteAll { src = ./tzdata.patch; inherit tzdata; }) ];
 
-  dontConfigure = true;
+    postPatch = ''
+      substituteInPlace Makefile \
+        --replace 'CRYSTAL_CONFIG_BUILD_COMMIT :=' 'CRYSTAL_CONFIG_BUILD_COMMIT ?=' \
+        --replace 'SOURCE_DATE_EPOCH :=' 'SOURCE_DATE_EPOCH ?='
+    '';
 
-  LLVM_CONFIG = "${llvm.dev}/bin/llvm-config";
-  CRYSTAL_CONFIG_TARGET = stdenv.targetPlatform.config;
-  CRYSTAL_CONFIG_BUILD_COMMIT = (builtins.substring 0 6 src.rev) + lib.optionalString release "-release";
-  SOURCE_DATE_EPOCH = "0";
-  preBuild = "export CRYSTAL_CACHE_DIR=$(mktemp -d)";
-  buildFlags = [ "interpreter=1" "threads=\${NIX_BUILD_CORES}" ] ++ lib.optionals release [ "release=1" ];
-  postBuild = "rm -rf $CRYSTAL_CACHE_DIR";
+    dontConfigure = true;
 
-  installPhase = ''
-    runHook preInstall
+    LLVM_CONFIG = "${llvm.dev}/bin/llvm-config";
+    CRYSTAL_CONFIG_TARGET = stdenv.targetPlatform.config;
+    CRYSTAL_CONFIG_BUILD_COMMIT = (builtins.substring 0 6 src.rev) + lib.optionalString release "-release";
+    SOURCE_DATE_EPOCH = "0";
+    preBuild = "export CRYSTAL_CACHE_DIR=$(mktemp -d)";
+    buildFlags = [ "interpreter=1" "threads=\${NIX_BUILD_CORES}" ] ++ lib.optionals release [ "release=1" ];
+    postBuild = "rm -rf $CRYSTAL_CACHE_DIR";
 
-    install -Dm755 ${shards}/bin/shards $bin/bin/shards
-    install -Dm755 .build/crystal $bin/bin/crystal
-    wrapProgram $bin/bin/crystal \
-       --suffix PATH : ${lib.makeBinPath [ pkg-config clang which ]} \
-       --suffix CRYSTAL_PATH : lib:$lib/crystal \
-       --suffix CRYSTAL_LIBRARY_PATH : ${ lib.makeLibraryPath (buildInputs) } \
-       --suffix PKG_CONFIG_PATH : ${openssl.dev}/lib/pkgconfig \
-       --suffix CRYSTAL_OPTS : "-Duse_pcre2" \
+    installPhase = ''
+      runHook preInstall
 
-    install -dm755 $lib/crystal
-    cp -r src/* $lib/crystal/
+      install -Dm755 ${shards}/bin/shards $bin/bin/shards
+      install -Dm755 .build/crystal $bin/bin/crystal
+      wrapProgram $bin/bin/crystal \
+         --suffix PATH : ${lib.makeBinPath [ pkg-config clang which ]} \
+         --suffix CRYSTAL_PATH : lib:$lib/crystal \
+         --suffix CRYSTAL_LIBRARY_PATH : ${ lib.makeLibraryPath (buildInputs) } \
+         --suffix PKG_CONFIG_PATH : ${openssl.dev}/lib/pkgconfig \
+         --suffix CRYSTAL_OPTS : "-Duse_pcre2" \
 
-    mkdir -p $out
-    ln -s $bin/bin $out/bin
-    ln -s $lib $out/lib
+      install -dm755 $lib/crystal
+      cp -r src/* $lib/crystal/
 
-    installShellCompletion etc/completion.fish etc/completion.bash etc/completion.zsh
+      mkdir -p $out
+      ln -s $bin/bin $out/bin
+      ln -s $lib $out/lib
 
-    runHook postInstall
-  '';
+      installShellCompletion etc/completion.fish etc/completion.bash etc/completion.zsh
 
-  dontStrip = true;
-}
+      runHook postInstall
+    '';
+
+    dontStrip = true;
+  }
+)
